237be38eb688af830e7f47ef8e2aedd1dbb54804
Sampo Kivistö <sampo.kivisto@innofactor.com>
2023-10-05 14:29:58 +0300

    Added cjs modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 export default {
   input: 'lib/index.js',
-  output: {
+  output: [{
     file: 'lib/index.mjs',
     format: 'esm',
     name: 'VuePropertyDecorator',
@@ -9,6 +9,15 @@ export default {
       '@havunen/vue2-class-component': 'VueClassComponent',
     },
     exports: 'named',
-  },
+  }, {
+    file: 'lib/index.cjs',
+    format: 'cjs',
+    name: 'VuePropertyDecorator',
+    globals: {
+      vue: 'Vue',
+      '@havunen/vue2-class-component': 'VueClassComponent',
+    },
+    exports: 'named',
+  }],
   external: ['vue', '@havunen/vue2-class-component', 'reflect-metadata'],
 }
